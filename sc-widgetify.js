9e0b5e0736f0b1b4eba5fbfbd27521e9848b047c
Tobias Bielohlawek <tobi@soundcloud.com>
2012-04-03 17:38:56 +0200

    added option to switch between json / jsonp, made https work

--- sc-widgetify.js
+++ sc-widgetify.js
@@ -13,6 +13,7 @@
     init : function( matches, options ) {
       options = $.extend(true, {
         https: false,
+        dataType: 'jsonp'
       }, options);
 
       /**
@@ -25,26 +26,28 @@
         $root = $this.is('ul') ? $this : $('<ul />').appendTo($this);
 
         $.each(matches, function(index, url) {
-          url = url.url || url;
+          url = (url.url || url).replace(/https?:/, options.https ? "https:" : "http:");
 
           // see http://developers.soundcloud.com/docs/oembed for more magic
           $.ajax({
-            url: "http://soundcloud.com/oembed",
-            dataType: 'jsonp',
+            url: (options.https ? "https:" : "http:") + "//soundcloud.com/oembed",
+            dataType: options.dataType,
             method: 'POST',
+            iframe: true,
             data: {
               url: url,
-              format: 'js',
+              format: (options.dataType == 'jsonp') ? 'js' : 'json',
             },
             success: function(result, status, xhr) {
-              if( $('li[data-sc-url="' + url + ' "]').length == 0 ) { // don't add duplicate URLs
-                var $node = $('<li data-sc-url="' + url + '" data-sc-height="' + result.height + '">' + result.html + '</li>').appendTo($root);
+              if( $root.find('li[data-sc-url="' + url + '"]').length == 0 ) { // don't add duplicate URLs
+                var iframe = result.html.replace(/https?/, options.https ? "https" : "http");
+                $node = $('<li data-sc-url="' + url + '" data-sc-height="' + result.height + '">' + iframe + '</li>').appendTo($root);
                 if( $.isFunction(options.callback) ) {
                   options.callback($node, result);
                 }
               }
             },
-            error: function() {
+            error: function(result) {
               // NOOP
             }
           });
